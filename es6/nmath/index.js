75753a47583adf107994c7cff0fba29e2bb0d0aa
shuangmu <shuangmu@qq.com>
2020-10-27 23:29:09 +0800

    自定义模块

---
+++ es6/nmath/index.js
@@ -0,0 +1,8 @@
+module.exports={
+    sum(a,b){
+        return a+b;
+    },
+    divde(a,b){
+        return a/b;
+    }
+}
---
+++ es6/test_nmath.js
@@ -0,0 +1,2 @@
+const nmath=require('nmath');
+console.log(nmath.sum(12,66));
