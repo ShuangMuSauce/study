d24efc2b0f2b39326b32abb4a7e2217e0520c303
shuangmu <shuangmu@qq.com>
2020-11-09 21:41:00 +0800

    保证洋葱模型

--- islang/app.js
+++ islang/app.js
@@ -1,35 +1,17 @@
 const Koa=require('koa')
-
 const app=new Koa()
 
-//应用程序对象  中间件
-
-//发送HTTP KOA 接受HTTP
-
-
-//注册   next下一个中间件函数
-// app.use((ctx,next)=>{
-//     //ctx 上下文   next下一个中间件函数
-//     console.log(1)
-//     const a=next()
-//     console.log(a) 
-//     console.log(2)
-// })
-
-// app.use(async (ctx,next)=>{
-//     //await==>阻塞当前线程 
-//     const axios=require('axios')
-//     const start=Date.now()
-//     const res=await axios.get('http://baidu.com')
-//     const end=Date.now()
-//     console.log(end-start) 
-// })
+app.use(async (ctx,next)=>{
+    await next()
+    const r=ctx.r //获得第二个中间件的res
+    console.log(r)
+})
 
 app.use(async (ctx,next)=>{
-    //await==>阻塞当前线程 
     const axios=require('axios')
     const res=await axios.get('http://baidu.com')
-    console.log(res) 
+    ctx.r=res
+    await next()
 })
 
 app.listen(3000)
