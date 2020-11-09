1d5a38040a7bf48bf614b25873a3b372b0cc1387
shuangmu <shuangmu@qq.com>
2020-11-09 21:18:48 +0800

    深入理解async和await

--- islang/app.js
+++ islang/app.js
@@ -8,22 +8,28 @@ const app=new Koa()
 
 
 //注册   next下一个中间件函数
-app.use((ctx,next)=>{
-    //ctx 上下文   next下一个中间件函数
-    console.log(1)
-    const a=next()
-    console.log(a) //下一个中间件不return ，输出Promise {undefined} ，
-                  //return 'abc',输出Promise {'abc'}
-    console.log(2)
+// app.use((ctx,next)=>{
+//     //ctx 上下文   next下一个中间件函数
+//     console.log(1)
+//     const a=next()
+//     console.log(a) 
+//     console.log(2)
+// })
+
+// app.use(async (ctx,next)=>{
+//     //await==>阻塞当前线程 
+//     const axios=require('axios')
+//     const start=Date.now()
+//     const res=await axios.get('http://baidu.com')
+//     const end=Date.now()
+//     console.log(end-start) 
+// })
+
+app.use(async (ctx,next)=>{
+    //await==>阻塞当前线程 
+    const axios=require('axios')
+    const res=await axios.get('http://baidu.com')
+    console.log(res) 
 })
-
-app.use((ctx,next)=>{
-    console.log(3)
-    console.log(4)
-    return 'abc'
-})
-
-//不加async，await依次输出，1，3，4，2，加上async，await可以保证万无一失
-
 
 app.listen(3000)
---
+++ islang/test.js
@@ -0,0 +1,4 @@
+async function f1(){
+    return 'hello'
+}
+console.log(f1())//强制包装成一个 Promise{'hello'}
