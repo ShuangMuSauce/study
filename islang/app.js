dc70fab9309791dcfabe89dcd4d95af8801ff295
shuangmu <shuangmu@qq.com>
2020-12-02 21:32:30 +0800

    实现路由自动

--- islang/app.js
+++ islang/app.js
@@ -1,10 +1,21 @@
 const Koa = require('koa')
+const requireDirectory = require('require-directory')
+const Router=require('koa-router')
 const book = require('./api/v1/book')
 const classic = require('./api/v1/classic')
 
 const app=new Koa()
 
-app.use(book.routes())
-app.use(classic.routes())
+const modules = requireDirectory(module,'./api',{
+    visit: whenLoadModule
+})
+
+function whenLoadModule(obj) {
+    if(obj instanceof Router) {
+        app.use(obj.routes())
+    }
+}
+// app.use(book.routes())
+// app.use(classic.routes())
 
 app.listen(3000)
