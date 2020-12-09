c1d377f4e00166ad87d5676af930d96f1f407e8c
shuangmu <shuangmu@qq.com>
2020-12-09 23:27:57 +0800

    参数获取

--- islang/app.js
+++ islang/app.js
@@ -1,8 +1,12 @@
 const Koa = require('koa')
+const parser = require('koa-bodyparser')
 const InitManager=require('./core/init')
 
 const app=new Koa()
 
+app.use(parser())
+
+process.cwd()
 InitManager.initCore(app)
 
 
--- islang/app/api/v1/classic.js
+++ islang/app/api/v1/classic.js
@@ -1,7 +1,11 @@
 const Router=require('koa-router')
 const router=new Router()
-
-router.get('/v1/classic/latest',(ctx,next)=>{
+// /v1/:id/classic/latest
+router.get('/v1/:id/classic/latest',(ctx,next)=>{
+    const path=ctx.params
+    const query=ctx.request.query
+    const headers=ctx.request.header
+    const body=ctx.request.body
     ctx.body={
         key: 'classic'
     }
