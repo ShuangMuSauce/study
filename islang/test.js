3b324c7e913469af302ef8f24ab9292e64ee2349
shuangmu <shuangmu@qq.com>
2020-12-13 15:27:59 +0800

    异常理论与异常

--- islang/test.js
+++ islang/test.js
@@ -1,4 +1,33 @@
-async function f1(){
-    return 'hello'
+//机制 监听任何异常
+
+function func1() {
+    try {
+        func2()
+    } catch (error) {
+        throw error
+    }
 }
-console.log(f1())//强制包装成一个 Promise{'hello'}
+
+function func2() {
+    try {
+        func3()
+    } catch (error) {
+        throw error
+    }
+}
+
+function func3() {
+    try {
+        1/0
+    } catch (error) {
+        throw error
+    }
+    return 'success'
+}
+
+//没有发生异常  正确返回结果
+// 发生了异常
+
+// 函数设计
+// 判断出异常 return false null
+// throw new Error 编程规范 throw
