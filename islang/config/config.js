97eb93f91b5183080388ce40a6f031df11815ada
shuangmu <shuangmu@qq.com>
2021-01-25 18:14:44 +0800

    Sequelize初始化配置与注意事项

--- islang/config/config.js
+++ islang/config/config.js
@@ -1,5 +1,12 @@
 module.exports = {
     //开发环境和生产环境
     //production  prod生产环境 dev开发环境
-    environment: 'dev'
+    environment: 'dev',
+    database:{
+        dbName:'islang',
+        host:'localhost',
+        port:3306,
+        user:'root',
+        password:'root'
+    }
 }
---
+++ islang/core/db.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+const {
+    dbName,
+    host,
+    port,
+    user,
+    password
+} = require('../config/config').database
+
+const sequelize = new Sequelize(dbName,user,password,{
+    dialect:'mysql',
+    host,
+    port,
+    logging:true,
+    timezone:'+08:00',
+    define:{
+
+    }
+})
+
+module.exports={
+    sequelize
+}
